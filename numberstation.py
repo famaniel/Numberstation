73edb1ff818d2036192d8f51889e332347904c38
ibug <ibug@congress>
2023-12-22 12:48:45 +0100

    add queued timer

--- numberstation.py
+++ numberstation.py
@@ -1,10 +1,12 @@
 import time
+import threading
+from random import random
 
 from bottle import post, request, route, run, static_file, view
+from queue import Queue
 
-from animation import Numbers, Chase
+from animation import Numbers, Chase, hsv_to_rgb
 from dmx import DMX, RGB
-
 
 @route('/')
 @view('index')
@@ -18,17 +20,40 @@ def number():
     r = request.query.r or 0
     g = request.query.g or 0
     b = request.query.b or 0
-    dmx.animation = Numbers(n, (r, g, b))
+    token = request.query.token or 0
+    if token == 'geheim':
+        priorityQueue.put(lambda: set_animation(Numbers(n, (r, g, b))))
+    else:
+        backgroundQueue.put(lambda: set_animation(Numbers(n, (r, g, b))))
 
 
+def set_animation(n: Numbers) -> None:
+    dmx.animation = n
+
+
+def queue_worker() -> None:
+    while True:
+        if not priorityQueue.empty():
+            func = priorityQueue.get()
+            func()
+        elif not backgroundQueue.empty():
+            func = backgroundQueue.get()
+            func()
+        else:
+            # dmx.animation = Chase((255,0,0))
+            set_animation(Numbers(f'{random()*100000000000:011.0f}', (hsv_to_rgb(random(), 1, 1))))
+        time.sleep(5)
+
+
+backgroundQueue = Queue(maxsize = 500)
+priorityQueue = Queue(maxsize = 250)
 dmx = DMX('127.0.0.1', maxchan=264)
 
 for digit in range(11):
     for segment in range(8):
         dmx._rgbs.append(RGB(dmx, 8 * 3 * digit + 3 * segment + 1, 0))
 
-dmx.animation = Chase((255, 0, 0))
-#dmx.animation = Numbers("abcdef ---------", (255, 0, 0))
+threading.Thread(target = queue_worker, daemon = True).start()
 
 dmx.start()
 
